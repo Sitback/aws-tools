3559482bd2a1b9d395081d3b7c53eb583bc7ab34
Lyndsay Roger <gombadi@users.noreply.github.com>
2014-07-08 14:06:48 +1000

    Update describe instances to use new layout

--- aws-describe-instances/aws-describe-instances.go
+++ aws-describe-instances/aws-describe-instances.go
@@ -9,55 +9,104 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"log"
 
 	"github.com/crowdmob/goamz/aws"
 	"github.com/crowdmob/goamz/ec2"
-	"github.com/sitback/go-ini"
+	"github.com/gombadi/go-ini"
 )
+
+// cmdline flag if we want verbose output
+var verbose bool
+
+// loadAWSCredentials function will first try and read from the env variables
+// and if nothing found then try from the standard file then from the command line
+func loadAWSCredentials(awsKey, awsSecret, regionName string) {
+
+	var awsSecretf, awsKeyf, regionNamef string
+	var ok bool
+
+	if len(os.Getenv("AWS_SECRET_ACCESS_KEY")) > 5 && len(os.Getenv("AWS_ACCESS_KEY_ID")) > 5 {
+		// all must be good so lets try these credentials
+		if verbose {
+			fmt.Printf("Info - Using AWS credentials from the environment\n")
+		}
+		return
+	}
+
+	// read the standard AWS ini file in case it is needed
+	iniFile, err := ini.LoadFile(os.Getenv("HOME") + "/.aws/credentials")
+
+	if err != nil {
+		// if we get an error with the standard name try the previous name
+		iniFile, err = ini.LoadFile(os.Getenv("HOME") + "/.aws/config")
+	}
+
+	awsSecretf, ok = iniFile.Get("default", "aws_secret_access_key")
+	if !ok {
+		// failed to read from file so last chance is command line
+		if awsSecret == "xxxx" {
+			log.Fatalf("Error - unable to find AWS Secret Key information\n")
+		} else {
+			os.Setenv("AWS_SECRET_ACCESS_KEY", awsSecret)
+		}
+
+	} else {
+		os.Setenv("AWS_SECRET_ACCESS_KEY", awsSecretf)
+	}
+
+	awsKeyf, ok = iniFile.Get("default", "aws_access_key_id")
+	if !ok {
+		// failed to read from file so last chance is command line
+		if awsKey == "xxxx" {
+			log.Fatalf("Error - unable to find AWS Access Key information\n")
+		} else {
+			os.Setenv("AWS_ACCESS_KEY_ID", awsKey)
+		}
+
+	} else {
+		os.Setenv("AWS_ACCESS_KEY_ID", awsKeyf)
+	}
+
+	// region is not a standard for the account so if not provided on
+	// command line then pull from the config file
+	if regionName == "xxxx" {
+		regionNamef, ok = iniFile.Get("default", "region")
+		if !ok {
+			log.Fatalf("Error - unable to find AWS Region information\n")
+		} else {
+			os.Setenv("AWS_ACCESS_REGION", regionNamef)
+		}
+	} else {
+		os.Setenv("AWS_ACCESS_REGION", regionName)
+	}
+	if verbose {
+		fmt.Printf("Info - Using AWS credentials from config file and connecting to region %s\n", os.Getenv("AWS_ACCESS_REGION"))
+	}
+
+}
+
+
+
 
 func main() {
 
 	// pointers to objects we use to talk to AWS
 	var e *ec2.EC2
 	// storage for commandline args
-	var regionName, awsKey, awsSecret string
-	var ok bool
+	var regionName, awsKey, awsSecret, awsRegion  string
 
 	flag.StringVar(&regionName, "r", "xxxx", "AWS Region to send request")
 	flag.StringVar(&awsKey, "k", "xxxx", "AWS Access Key")
 	flag.StringVar(&awsSecret, "s", "xxxx", "AWS Secret key")
 	flag.Parse()
 
-	// read the standard AWS ini file in case it is needed
-	iniFile, err := ini.LoadFile(os.Getenv("HOME") + "/.aws/config")
 
-	// use any values not supplied on the command line
-	if regionName == "xxxx" {
-		regionName, ok = iniFile.Geti("default", "region")
-		if !ok {
-			fmt.Printf("Error - unable to find AWS Region information\n")
-			os.Exit(1)
-		}
-	}
+	// load the AWS credentials from the environment or from the standard file
+	loadAWSCredentials(awsKey, awsSecret, regionName)
 
-	//  read secret key from command line or ini file
-	// if either secret key or access key are not provided then read all info from ini file
-	if awsSecret == "xxxx" || awsKey == "xxxx" {
-		awsSecret, ok = iniFile.Geti("default", "AWS_SECRET_ACCESS_KEY")
-		if !ok {
-			fmt.Printf("Error - unable to find AWS Secret Key information\n")
-			os.Exit(1)
-		}
-
-		awsKey, ok = iniFile.Geti("default", "AWS_ACCESS_KEY_ID")
-		if !ok {
-			fmt.Printf("Error - unable to find AWS Access Key information\n")
-			os.Exit(1)
-		}
-	}
-	// store auth info in environment
-	os.Setenv("AWS_SECRET_ACCESS_KEY", awsSecret)
-	os.Setenv("AWS_ACCESS_KEY_ID", awsKey)
+	// pull the region from the environment
+	awsRegion = os.Getenv("AWS_ACCESS_REGION")
 
 	// Pull the access details from environment variables
 	// AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
@@ -68,7 +117,7 @@ func main() {
 	}
 
 	// create the objects we will use to talk to AWS
-	switch regionName {
+	switch awsRegion {
 	case "eu-west-1":
 		e = ec2.New(auth, aws.EUWest)
 	case "sa-east-1":
@@ -86,7 +135,7 @@ func main() {
 	case "ap-southeast-2":
 		e = ec2.New(auth, aws.APSoutheast2)
 	default:
-		fmt.Printf("Error - Sorry I can not find the url endpoint for region %s\n", regionName)
+		fmt.Printf("Error - Sorry I can not find the url endpoint for region %s\n", awsRegion)
 		os.Exit(1)
 	}
 

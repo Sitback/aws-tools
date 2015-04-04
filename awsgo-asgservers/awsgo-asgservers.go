ee47ca2a5d00b66dc94ddf4f877d87ebd51ec53b
Lyndsay Roger <gombadi@users.noreply.github.com>
2015-04-04 15:34:04 +1300

    Update code to new AWS Labs based SDK

---
+++ awsgo-asgservers/awsgo-asgservers.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/service/autoscaling"
+	"github.com/awslabs/aws-sdk-go/service/ec2"
+)
+
+func main() {
+
+	// storage for commandline args
+	var asgName string
+
+	flag.StringVar(&asgName, "a", "", "AWS Auto scale group name")
+	flag.Parse()
+
+	// Get details of current Auto Scale Groups
+	// Create empty auto scale group list and append any group name on the command line
+	asgNames := []*string{}
+	if len(asgName) > 0 {
+		asgNames = append(asgNames, &asgName)
+	}
+
+	asgi := autoscaling.DescribeAutoScalingGroupsInput{AutoScalingGroupNames: asgNames}
+
+	// Create an Autoscaling service object
+	// config values keys, sercet key & region read from environment
+	svcAs := autoscaling.New(&aws.Config{})
+	resp, err := svcAs.DescribeAutoScalingGroups(&asgi)
+
+	if awserr := aws.Error(err); awserr != nil {
+		// A service error occurred.
+		fmt.Println("AWS Error:", awserr.Code, awserr.Message)
+	} else if err != nil {
+		// A non-service error occurred.
+		panic(err)
+	}
+
+	// if no asg name provided then display current asg names and exit
+	if len(asgName) == 0 {
+		fmt.Println("No Autoscaling Group Name provided. Current Groups:")
+
+		for asGroup := range resp.AutoScalingGroups {
+			fmt.Printf("%s\n", *resp.AutoScalingGroups[asGroup].AutoScalingGroupName)
+		}
+		fmt.Println("")
+
+		os.Exit(0)
+	}
+
+	if len(resp.AutoScalingGroups) < 1 {
+		fmt.Printf("No Auto Scale Group info found for %s.\n", *asgNames[0])
+		os.Exit(1)
+	}
+
+	instanceSlice := []*string{}
+
+	// extract the instanceid's from the auto scale details and append to a slice
+	for asGroup := range resp.AutoScalingGroups {
+		for instance := range resp.AutoScalingGroups[asGroup].Instances {
+			instanceSlice = append(instanceSlice, resp.AutoScalingGroups[asGroup].Instances[instance].InstanceID)
+		}
+	}
+
+	if len(instanceSlice) < 1 {
+		fmt.Printf("No instances in auto scale group %s.\n", *asgNames[0])
+		os.Exit(1)
+	}
+
+	ec2i := ec2.DescribeInstancesInput{InstanceIDs: instanceSlice}
+
+	// Create an EC2 service object
+	// config values keys, sercet key & region read from environment
+	svcEc2 := ec2.New(&aws.Config{})
+	respEc2, err := svcEc2.DescribeInstances(&ec2i)
+
+	if awserr := aws.Error(err); awserr != nil {
+		// A service error occurred.
+		fmt.Println("AWS Error:", awserr.Code, awserr.Message)
+	} else if err != nil {
+		// A non-service error occurred.
+		panic(err)
+	}
+
+	// extract the private ip address from the instance struct stored in the reservation
+	for reservation := range respEc2.Reservations {
+		for instance := range respEc2.Reservations[reservation].Instances {
+			fmt.Printf("%s\n",
+				*respEc2.Reservations[reservation].Instances[instance].PrivateIPAddress)
+		}
+	}
+
+}
